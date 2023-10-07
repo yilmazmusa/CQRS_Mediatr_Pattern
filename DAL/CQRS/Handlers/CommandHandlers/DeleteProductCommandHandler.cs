ee6cfd63fdb5f69c6aa01b8f0554f6c41b2ae901
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-10-08 02:11:13 +0300

    CQRS için Command, Query ve Handler yapıları oluşturuldu.

---
+++ DAL/CQRS/Handlers/CommandHandlers/DeleteProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using DAL.CQRS.Commands.Request;
+using DAL.CQRS.Commands.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.CQRS.Handlers.CommandHandlers
+{
+    public class DeleteProductCommandHandler
+    {
+        public DeleteProductCommandResponse DeleteProduct(DeleteProductCommandRequest deleteProductCommandRequest)
+        {
+            var deleteProduct = ApplicationDBContext.Products.FirstOrDefault(p => p.Id == deleteProductCommandRequest.Id);
+
+            ApplicationDBContext.Products.Remove(deleteProduct);
+
+            return new DeleteProductCommandResponse
+            {
+                IsSucces = true
+            };
+        }
+    }
+}
